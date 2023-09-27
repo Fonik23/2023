29c492535487f8f94d30fd0bf9ac59ef5db6d378
Fonik23 <busbeet88@gmail.com>
2023-09-27 22:47:03 +0300

    feat: add template card at html  (Wed, Sep 27, 2023 10:47:03 PM)

--- movie-app/scripts/index.js
+++ movie-app/scripts/index.js
@@ -2,11 +2,16 @@ const input = document.querySelector('input')
 
 const key = '6ef21418'
 
-const url = `https://www.omdbapi.com/?s=Star+wars&apikey=${key}`
 
 async function getData (){
+    const url = `https://www.omdbapi.com/?s=Star+wars&apikey=${key}`
     const resp = await fetch(url)
-    const data = resp.json()
+    const data = await resp.json()
     console.log(data)
 }
 getData()
+
+
+
+
+
