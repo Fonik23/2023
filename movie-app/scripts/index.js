0ff076769f520f6ab55587c82494fa7f9418541b
Fonik23 <busbeet88@gmail.com>
2023-09-28 01:26:20 +0300

    feat: add start movies (Thu, Sep 28, 2023 01:26:20 AM)

--- movie-app/scripts/index.js
+++ movie-app/scripts/index.js
@@ -1,3 +1,5 @@
+const movies = document.querySelector('.movies')
+
 const input = document.querySelector('input')
 
 const key = '6ef21418'
@@ -8,10 +10,27 @@ async function getData (){
     const resp = await fetch(url)
     const data = await resp.json()
     console.log(data)
+    startMovie(data)
 }
 getData()
 
-
+const startMovie = (data) => {
+    
+    data.Search.forEach(element => {
+        const item = document.createElement('div')
+        item.innerHTML = ` 
+        <div class="movie">
+            <div class="movie__poster" style="background-image: url('${element.Poster}');"> </div>
+            <div class="movie__box">
+                <h4 class="movie__title">${element.Title}</h4>
+                <p class="movie__year">Year:<span class="movie__year-value">${element.Year}</span></p>
+            </div>
+        </div>`
+        movies.appendChild(item)
+    
+    
+    });
+}
 
 
 
