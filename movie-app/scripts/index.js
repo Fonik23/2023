206bfbf9153731542dd89c25d56dbcbacd85d459
Fonik23 <busbeet88@gmail.com>
2023-09-30 20:24:15 +0300

    refactor: delete commented code   (Sat, Sep 30, 2023 08:24:15 PM)

--- movie-app/scripts/index.js
+++ movie-app/scripts/index.js
@@ -48,30 +48,6 @@ const startMovie = (data) => {
 }
 
 
-
-
-// button.addEventListener('click', (event) => {
-//     if(input.value !== ''){
-//         event.preventDefault()
-//         const url = `https://www.omdbapi.com/?s=${input.value}&apikey=${key}`
-//         console.log(url)
-//         getData(url)
-//     } else {
-//         alert('aaaaa')
-//     }
-    
-// })
-
-
-// input.addEventListener('keydown', (event) => {
-//     if(event.key === 'Enter'){
-//         event.preventDefault()
-//         const url = `https://www.omdbapi.com/?s=${input.value}&apikey=${key}`
-//         console.log(url)
-//         getData(url)
-//     }
-// })
-
 const btnCheck = () => {
     button.addEventListener('click', (event)=>{
         if(event.target.contains('button')){
@@ -97,8 +73,6 @@ form.addEventListener('submit', (event) => {
     console.log(event)
     if(input.value !== ''){
         if(btnCheck || keyboardCheck){
-            console.log(url)
-            // getData(url)
             movies.innerHTML = ''
             getTotalResults(input.value)
         }
